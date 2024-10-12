47bbfd55ff292f294d9f9aff1256ffafa06de123
Dieudonne <iyabivuzed@gmail.com>
2024-10-12 20:04:03 +0200

    modified the notification component and added the styles on the scrollbar

--- src/app/components/dashboard/bars/Notifications.tsx
+++ src/app/components/dashboard/bars/Notifications.tsx
@@ -11,7 +11,7 @@ const Notifications = ({ notification }: NavBarProp) => {
     <div
       className={`${
         notification ? "opacity-100 " : "opacity-0"
-      } rounded-md flex transition-all flex-col duration-400 bg-base-200 shadow-md h-[550px] w-[400px] max-sm:w-full absolute top-[70px] max-sm:right-0 right-3 px-2 overflow-hidden
+      } rounded-md flex transition-all flex-col duration-400 bg-base-200 shadow-md h-[500px] w-[400px] max-sm:w-full absolute top-[70px] max-sm:right-0 right-3 px-2 overflow-scroll
       `}
     >
       <div className="flex justify-between gap-2 items-center w-full py-4">
@@ -27,6 +27,34 @@ const Notifications = ({ notification }: NavBarProp) => {
             <span className="badge badge-xs badge-primary indicator-item"></span>
           </div>
           <p className="text-blue-500 text-sm font-normal"> 1m ago </p>
+        </div>
+        <div className="bg-base-100 px-3 py-2 rounded-md">
+          <p> Niyibikora Obed is present in the school </p>
+          <p> 1h ago </p>
+        </div>
+        <div className="bg-base-100 px-3 py-2 rounded-md">
+          <p> Niyibikora Obed is present in the school </p>
+          <p> 1h ago </p>
+        </div>
+        <div className="bg-base-100 px-3 py-2 rounded-md">
+          <p> Niyibikora Obed is present in the school </p>
+          <p> 1h ago </p>
+        </div>
+        <div className="bg-base-100 px-3 py-2 rounded-md">
+          <p> Niyibikora Obed is present in the school </p>
+          <p> 1h ago </p>
+        </div>
+        <div className="bg-base-100 px-3 py-2 rounded-md">
+          <p> Niyibikora Obed is present in the school </p>
+          <p> 1h ago </p>
+        </div>
+        <div className="bg-base-100 px-3 py-2 rounded-md">
+          <p> Niyibikora Obed is present in the school </p>
+          <p> 1h ago </p>
+        </div>
+        <div className="bg-base-100 px-3 py-2 rounded-md">
+          <p> Niyibikora Obed is present in the school </p>
+          <p> 1h ago </p>
         </div>
         <div className="bg-base-100 px-3 py-2 rounded-md">
           <p> Niyibikora Obed is present in the school </p>
