2742aa4eff2985ad365262f12d582e6f7b62d6f0
Dieudonne <iyabivuzed@gmail.com>
2024-10-12 23:19:44 +0200

    Added a pre-loader

--- src/app/components/dashboard/Dashboard.tsx
+++ src/app/components/dashboard/Dashboard.tsx
@@ -4,8 +4,12 @@ import React, { useState } from "react";
 import Navbar from "./bars/Navbar";
 import Sidebar from "./bars/Sidebar";
 import Activity from "../activities/Activity";
+import Head from "next/head";
 
 const Dashboard = () => {
+
+  const pageTitle = "Dashboard - Attendance.";
+
   // Retrieve toggle state from localStorage or set it to false by default
   const [clicked, setClicked] = useState<boolean>(() => {
     if (typeof window !== "undefined") {
@@ -19,26 +23,35 @@ const Dashboard = () => {
   const handleToggle = () => {
     const newState = !clicked;
     setClicked(newState);
-    localStorage.setItem("sidebar-toggled", JSON.stringify(newState)); 
+    localStorage.setItem("sidebar-toggled", JSON.stringify(newState));
   };
 
   return (
-    <div className="flex h-screen w-full ">
-      <div
-        className={`${
-          clicked ? "w-0" : "w-[200px]"
-        } h-full transition-all duration-300 bg-base-100 shadow-lg fixed top-0 left-0 overflow-hidden border-r-2 border-opacity-10 border-slate-500`}
-      >
-        <Sidebar />
-      </div>
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content="Dashboard - Attendance." />
+      </Head>
 
-      <div className={`${clicked ? "ml-0 w-full" : "ml-[200px] flex-1 py-3 "}`}>
-        <div className="shadow-md">
-          <Navbar handleToggle={handleToggle} />
+      <div className="flex h-screen w-full ">
+        <div
+          className={`${
+            clicked ? "w-0" : "w-[200px]"
+          } h-full transition-all duration-300 bg-base-100 shadow-lg fixed top-0 left-0 overflow-hidden border-r-2 border-opacity-10 border-slate-500`}
+        >
+          <Sidebar />
         </div>
-        <Activity />
+
+        <div
+          className={`${clicked ? "ml-0 w-full" : "ml-[200px] flex-1 py-3 "}`}
+        >
+          <div className="shadow-md">
+            <Navbar handleToggle={handleToggle} />
+          </div>
+          <Activity />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,48 @@
 "use client";
+import { useEffect, useState } from "react";
 import Home from "./components/home/Home";
 import Theme from "./helpers/Themes";
+import Head from "next/head";
+import Loader from "./helpers/Loader";
 
 export default function HomePage() {
+
+  const [loading, setLoading] = useState(true); // Set loading to true initially
+
+  // Simulate loading delay
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Hide preloader after 2 seconds
+    }); // Adjust the time as necessary
+
+    return () => clearTimeout(timer); // Clean up timer on unmount
+  }, []);
+
+  const pageTitle = "Home - Attendance.";
+
   return (
-    <div className="bg-base-100 min-h-screen flex flex-col items-center justify-center">
-      <div className="absolute top-3 right-12">
-        <Theme />
-      </div>
-      <Home />
-      <aside className="opacity-50 my-2 text-sm px-2 text-center">
-        <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by
-          CLASSTAP Industries Ltd
-        </p>
-      </aside>
-    </div>
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content="Home - Attendance." />
+      </Head>
+
+      {loading ? ( // Conditional rendering based on loading state
+        <Loader />
+      ) : (
+        <div className="bg-base-100 min-h-screen flex flex-col items-center justify-center">
+          <div className="absolute top-3 right-12">
+            <Theme />
+          </div>
+          <Home />
+          <aside className="opacity-50 my-2 text-sm px-2 text-center">
+            <p>
+              Copyright © {new Date().getFullYear()} - All rights reserved by
+              CLASSTAP Industries Ltd
+            </p>
+          </aside>
+        </div>
+      )}
+    </>
   );
 }
