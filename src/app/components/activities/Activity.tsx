ecbd376b276cf7ccf7419f3c1f874c934290cb40
Dieudonne <iyabivuzed@gmail.com>
2024-10-12 10:26:21 +0200

    Added a hard-codeddata in statistics component

--- src/app/components/activities/Activity.tsx
+++ src/app/components/activities/Activity.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Students from "./Students";
+import Statistics from "./Statistics";
 
 const Activity = () => {
   return (
@@ -9,6 +10,9 @@ const Activity = () => {
       <div className="text-center mt-8 ">
         <h1 className="text-3xl font-semibold">Friday, Oct 11, 2024 Active attendances</h1>
       </div>
+
+      {/* Total of present students, late, and absent */}
+      <Statistics/>
 
       {/* Provisionalll */}
       <Students/>
---
+++ src/app/components/activities/Statistics.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import React from 'react'
+
+const Statistics = () => {
+  return (
+    <div className="flex justify-center items-center gap-3 mt-12 px-5 max-sm:flex-col">
+      <div className="bg-base-100 w-full border-2 border-opacity-5 shadow-md">
+        <h1 className="p-3 bg-base-200 text-center text-xl font-semibold">
+          Total Present
+        </h1>
+        <div className="p-4 text-center">
+          <h1 className="text-2xl">350</h1>
+        </div>
+      </div>
+      {/* Late */}
+      <div className="bg-base-100 w-full border-1 border-opacity-15 shadow-md">
+        <h1 className="p-3 bg-base-200 text-center text-xl font-semibold">
+          Total Late
+        </h1>
+        <div className="p-4 text-center">
+          <h1 className="text-2xl">50</h1>
+        </div>
+      </div>
+      {/* Absent */}
+      <div className="bg-base-100 w-full border-1 border-opacity-15 shadow-md">
+        <h1 className="p-3 bg-base-200 text-center text-xl font-semibold">
+          Total Absent
+        </h1>
+        <div className="p-4 text-center">
+          <h1 className="text-2xl">100</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Statistics
